ccae8fbc181451365693e5bbf0cff14a87276352
socrastein <31764357+socrastein@users.noreply.github.com>
2025-02-14 11:44:40 -0700

    Incorporate <ul> list of features when displaying projects.

--- src/projectPage/buildProjectPage.js
+++ src/projectPage/buildProjectPage.js
@@ -18,6 +18,22 @@ export default function buildProjectPage(projectObj) {
   titleContainer.append(icon);
   titleContainer.append(title);
 
+  const featuresContainer = document.createElement("div");
+  featuresContainer.classList.add("projectPageFeaturesContainer");
+
+  const featureList = document.createElement("ul");
+  featureList.classList.add("projectPageFeatureList");
+
+  projectObj.listOfFeatures.forEach((feature) => {
+    if (feature !== "") {
+      const listItem = document.createElement("li");
+      listItem.textContent = feature;
+      featureList.append(listItem);
+    }
+  });
+
+  featuresContainer.appendChild(featureList);
+
   const descriptionContainer = document.createElement("div");
   descriptionContainer.classList.add("projectPageDescription");
   descriptionContainer.textContent = projectObj.descriptionLong;
@@ -34,6 +50,7 @@ export default function buildProjectPage(projectObj) {
   screenShot.src = projectObj.image;
 
   appContainer.appendChild(titleContainer);
+  appContainer.appendChild(featuresContainer);
   appContainer.append(descriptionContainer);
   appContainer.append(linkToProject);
   appContainer.append(screenShot);
