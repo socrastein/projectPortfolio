e32c09535e967f91dc3ad26e51ee289e27da45e2
socrastein <31764357+socrastein@users.noreply.github.com>
2025-02-13 23:56:46 -0700

    Write buildProjectPage function
    Display detailed description and screenshot of project on dedicated page

--- src/projectPage/buildProjectPage.js
+++ src/projectPage/buildProjectPage.js
@@ -1,23 +1,53 @@
+import "./projectPage.css"
+
 const appContainer = document.querySelector("#app");
 
 export default function buildProjectPage(projectObj) {
-    appContainer.innerHTML = '';
+  appContainer.innerHTML = "";
 
-    const mainContainer = document.createElement("div");
-    mainContainer.classList.add("pageMainContainer");
+  const titleContainer = document.createElement("div");
+  titleContainer.classList.add("projectPageTitleContainer");
 
-    mainContainer.innerHTML = '<h1>This is one of my projects</h1>';
+  const icon = document.createElement("img");
+  icon.classList.add("projectPageIcon");
+  icon.src = projectObj.icon;
 
-    appContainer.appendChild(mainContainer);
-};
+  const title = document.createElement("h1");
+  title.classList.add("projectPageTitle");
+  title.textContent = projectObj.name;
+
+  titleContainer.append(icon);
+  titleContainer.append(title);
+
+  const descriptionContainer = document.createElement("div");
+  descriptionContainer.classList.add("projectPageDescription");
+  descriptionContainer.textContent = projectObj.descriptionLong;
+
+  const linkToProject = document.createElement("button");
+  linkToProject.classList.add("projectPageButton");
+  linkToProject.textContent = "Check It Out";
+  linkToProject.addEventListener("click", () => {
+    window.open(`${projectObj.link}`, "_blank");
+  });
+
+  const screenShot = document.createElement("img");
+  screenShot.classList.add("projectPageScreenShot");
+  screenShot.src = projectObj.image;
+
+  appContainer.appendChild(titleContainer);
+  appContainer.append(descriptionContainer);
+  appContainer.append(linkToProject);
+  appContainer.append(screenShot);
+}
 
 function projectTitle(text) {
-    const title = document.createElement("h1");
-    title.classList.add("projectTitle");
-    title.textContent = text;
+  const title = document.createElement("h1");
+  title.classList.add("projectTitle");
+  title.textContent = text;
 
-    return title;
+  return title;
 }
 
 const videoContainer = document.createElement("div");
-    videoContainer.innerHTML = '<iframe src="https://drive.google.com/file/d/166DNUGfodhgaeYB_yTEHiqg_KJnk3x0L/preview" width="640" height="480" allow="autoplay"></iframe>';
+videoContainer.innerHTML =
+  '<iframe src="https://drive.google.com/file/d/166DNUGfodhgaeYB_yTEHiqg_KJnk3x0L/preview" width="640" height="480" allow="autoplay"></iframe>';
