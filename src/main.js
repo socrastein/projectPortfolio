1544adda38355cdffab6aadbce3b98de1c8dfbd0
socrastein <31764357+socrastein@users.noreply.github.com>
2025-05-02 19:20:52 -0600

    Change createRouter to createWebHashHistory due to GH Pages 404 on projectView

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import './styles/animations.css';
 import './styles/text.css';
 
 import { createApp } from 'vue';
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 
 import App from './components/App.vue';
 
@@ -27,8 +27,16 @@ const routes = [
 ];
 const router = createRouter(
     {
-        // Prepends /portfolioProject to page paths
-        history: createWebHistory(import.meta.env.BASE_URL), routes,
+        
+        history: createWebHashHistory(), routes: [
+
+            { path: '/', name: 'home', component: HomeView },
+            { path: '/about', name: 'about', component: AboutView },
+            { path: '/projects', name: 'projects', component: ProjectsView },
+            { path: '/projects/:projectName', component: ProjectView, props: true },
+
+        ],
+
         scrollBehavior(to, from, savedPosition) {
             // Scroll to top of page
             return { top: 0 };
